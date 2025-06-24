0ab2156c969fae0a242562a005a43a1a5a6ecaa1
YawFrimpong139 <yawzealow@gmail.com>
2025-06-24 09:31:57 +0100

    chore: Initial project setup

---
+++ src/main/java/org/codewithzea/trackerboost/TrackerBoostApplication.java
@@ -0,0 +1,13 @@
+package org.codewithzea.trackerboost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerBoostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackerBoostApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/codewithzea/trackerboost/TrackerBoostApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codewithzea.trackerboost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerBoostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
